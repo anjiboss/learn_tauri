c61d3212f473ce112b50b1313690d09911bb51af
anjiboss <nhu.nhu.face@gmail.com>
2022-12-30 22:12:58 +0900

    basic counter app with state on rust side

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,36 @@
     windows_subsystem = "windows"
 )]
 
+use std::sync::{Arc, Mutex};
+
+use tauri::State;
+
+#[derive(Default)]
+struct Counter(Arc<Mutex<i32>>);
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn log_console(phrase: &str) {
+    println!("{}", phrase);
+}
+
+#[tauri::command]
+fn count_many(times: i32, counter: State<'_, Counter>) -> i32 {
+    let mut value = counter.0.lock().unwrap();
+    *value += times;
+    println!("Counter: {}", *value);
+    return *value;
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(Counter(Default::default()))
+        .invoke_handler(tauri::generate_handler![greet, log_console, count_many])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { invoke } from "@tauri-apps/api/tauri";
+import {
+  isRegistered,
+  register,
+  unregister,
+} from "@tauri-apps/api/globalShortcut";
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
+let counterElm: HTMLElement | null;
+
+registerNewCmd();
 
 async function greet() {
   if (greetMsgEl && greetInputEl) {
@@ -15,7 +23,31 @@ async function greet() {
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
+  counterElm = document.querySelector("#counter");
+
   document
     .querySelector("#greet-button")
     ?.addEventListener("click", () => greet());
 });
+
+async function registerNewCmd() {
+  const registered = await isRegistered("CommandOrControl+Shift+U");
+  const registering = async () => {
+    await register("CommandOrControl+Shift+U", async () => {
+      const count: number = await invoke("count_many", { times: 5 });
+      if (counterElm) {
+        counterElm.textContent = count.toString();
+      }
+    });
+  };
+  if (!registered) {
+    console.log("Registering shortcut");
+    registering();
+  } else {
+    // unregistering command
+    invoke("log_console", { phrase: "Unregistering shortcut" });
+    await unregister("CommandOrControl+Shift+U");
+    invoke("log_console", { str: "Reigster again" });
+    registering();
+  }
+}
